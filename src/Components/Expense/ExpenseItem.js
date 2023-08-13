9641bbc635b45a3c2d61d0d81b6928c85475a92c
Manpreet <mpsingh17@outlook.co.nz>
2023-08-13 18:20:57 +1200

    Cleanup ExpenseItem component.

--- src/Components/Expense/ExpenseItem.js
+++ src/Components/Expense/ExpenseItem.js
@@ -1,24 +1,15 @@
-import { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  const buttonClickHandler = () => {
-    setTitle("updated");
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
 
       <div className="expense-item__description">
-        <h1>{title}</h1>
+        <h1>{props.title}</h1>
         <p className="expense-item__price">${props.amount}</p>
-
-        <button onClick={buttonClickHandler}>Update Title</button>
       </div>
     </Card>
   );
